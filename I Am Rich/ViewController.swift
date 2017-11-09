ab035002e05c4cdc7d21595b9e4fe01eb60ca2a7
Thando Mini <thandomini@Thandos-MacBook-Air.local>
2017-11-09 16:01:58 +0200

    First and final commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Thando Mini on 2017/09/20.
+//  Copyright © 2017 Thando Mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
